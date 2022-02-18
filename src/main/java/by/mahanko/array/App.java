9c5866a27f29200e7d31106751b2a6719c8cffd6
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-19 02:40:49 +0300

    Time checking(merge sort much faster)

--- src/main/java/by/mahanko/array/App.java
+++ src/main/java/by/mahanko/array/App.java
@@ -3,13 +3,17 @@ package by.mahanko.array;
 import by.mahanko.array.entity.CustomArray;
 import by.mahanko.array.service.ArraySortService;
 
+
 public class App {
     public static void main(String[] args) {
-        CustomArray array = new CustomArray(9,8,7,6,5,4,3,2,1,0);
+        long time = System.nanoTime();
+        CustomArray array = new CustomArray();
         ArraySortService sorter = new ArraySortService();
         try {
             sorter.mergeSort(array);
-            System.out.println(array.toString());
+            System.out.println(array);
+            long end = System.nanoTime();
+            System.out.println(end - time);
         }
         catch (Exception exception) {
             System.out.println(exception.getCause());
