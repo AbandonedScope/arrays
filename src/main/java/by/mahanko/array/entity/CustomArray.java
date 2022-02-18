730db3d8d586b133bc81c696824e1b2fa511dfa2
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-19 00:07:52 +0300

    getArray, getElement, setElement methods added

--- src/main/java/by/mahanko/array/entity/CustomArray.java
+++ src/main/java/by/mahanko/array/entity/CustomArray.java
@@ -3,7 +3,31 @@ package by.mahanko.array.entity;
 public class CustomArray {
     private int[] array;
 
-    public CustomArray(int... array){
+    public CustomArray(int... array) {
         this.array = array;
+    }
+
+    public int length() {
+        return array.length;
+    }
+
+    public int[] getArray() {
+        return array.clone();
+    }
+
+    public int getElement(int index) throws ArrayException {
+        if (index < 0 || index>= array.length) {
+            throw new ArrayException();
+        }
+
+        return array[index];
+    }
+
+    public void setElement(int index, int value) throws ArrayException {
+        if (index < 0 || index>= array.length) {
+            throw new ArrayException();
+        }
+
+        array[index] = value;
     }
 }
