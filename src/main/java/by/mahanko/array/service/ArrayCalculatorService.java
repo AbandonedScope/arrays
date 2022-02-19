3f676355a7028cc7644620b0f759dd80236608c1
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-19 13:01:21 +0300

    countElementsSum and countAverage methods added

---
+++ src/main/java/by/mahanko/array/service/ArrayCalculatorService.java
@@ -0,0 +1,19 @@
+package by.mahanko.array.service;
+
+import by.mahanko.array.entity.CustomArray;
+
+public class ArrayCalculatorService {
+    public int countElementsSum(CustomArray array) {
+        int sum = 0;
+        for (int element : array.getArray()) {
+            sum += element;
+        }
+
+        return sum;
+    }
+
+    public double countAverage(CustomArray array) {
+        double average = countElementsSum(array) / (double)array.length();
+        return average;
+    }
+}
