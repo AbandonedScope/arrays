aff08d24a4a7d552704ad7f742600acab37c6c8d
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-19 15:27:57 +0300

    logging added

--- src/main/java/by/mahanko/array/service/ArrayCalculateService.java
+++ src/main/java/by/mahanko/array/service/ArrayCalculateService.java
@@ -2,8 +2,12 @@ package by.mahanko.array.service;
 
 import by.mahanko.array.exception.CustomException;
 import by.mahanko.array.entity.CustomArray;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ArrayCalculateService {
+    static Logger logger = LogManager.getLogger(ArrayCalculateService.class.getName());
+
     public int countElementsSum(CustomArray array) throws CustomException {
         if (array.length() == 0) {
             throw new CustomException();
@@ -12,12 +16,13 @@ public class ArrayCalculateService {
         for (int element : array.getArray()) {
             sum += element;
         }
-
+        logger.info("Elements sum - " + sum);
         return sum;
     }
 
     public double countAverage(CustomArray array) throws CustomException {
         double average = countElementsSum(array) / (double) array.length();
+        logger.info("Array's average - " + average);
         return average;
     }
 }
