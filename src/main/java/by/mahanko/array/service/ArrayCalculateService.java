343d06e9770557c9f8a73d3b3085e284dd2fb405
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-19 13:49:12 +0300

    ArrayException throwing added

--- src/main/java/by/mahanko/array/service/ArrayCalculateService.java
+++ src/main/java/by/mahanko/array/service/ArrayCalculateService.java
@@ -1,9 +1,13 @@
 package by.mahanko.array.service;
 
+import by.mahanko.array.entity.ArrayException;
 import by.mahanko.array.entity.CustomArray;
 
 public class ArrayCalculateService {
-    public int countElementsSum(CustomArray array) {
+    public int countElementsSum(CustomArray array) throws ArrayException {
+        if (array.length() == 0) {
+            throw new ArrayException();
+        }
         int sum = 0;
         for (int element : array.getArray()) {
             sum += element;
@@ -12,8 +16,8 @@ public class ArrayCalculateService {
         return sum;
     }
 
-    public double countAverage(CustomArray array) {
-        double average = countElementsSum(array) / (double)array.length();
+    public double countAverage(CustomArray array) throws ArrayException {
+        double average = countElementsSum(array) / (double) array.length();
         return average;
     }
 }
