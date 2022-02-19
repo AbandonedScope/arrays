a51e0859dcaeb5d7f0a3c5bbec0ec38610e0fad4
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-19 13:55:00 +0300

    replaceAllNegativeElements, replaceAllElementInRegion methods added

--- src/main/java/by/mahanko/array/service/ArrayManipulationService.java
+++ src/main/java/by/mahanko/array/service/ArrayManipulationService.java
@@ -1,4 +1,32 @@
 package by.mahanko.array.service;
 
+import by.mahanko.array.exception.CustomException;
+import by.mahanko.array.entity.CustomArray;
+
 public class ArrayManipulationService {
+    public void replaceAllNegativeElements(CustomArray array, int valueToReplaceWith) throws CustomException {
+        if (array.length() == 0) {
+            throw  new CustomException();
+        }
+
+        int length = array.length();
+        for (int i = 0; i < length; i++) {
+            if (array.getElement(i) < 0) {
+                array.setElement(i, valueToReplaceWith);
+            }
+        }
+    }
+
+    public void replaceAllElementInRegion(CustomArray array, int lowerBound, int uppedBound, int valueToReplaceWith) throws CustomException {
+        if (array.length() == 0) {
+            throw  new CustomException();
+        }
+
+        int length = array.length();
+        for (int i = 0; i < length; i++) {
+            if (array.getElement(i) >= lowerBound && array.getElement(i) <= uppedBound) {
+                array.setElement(i, valueToReplaceWith);
+            }
+        }
+    }
 }
