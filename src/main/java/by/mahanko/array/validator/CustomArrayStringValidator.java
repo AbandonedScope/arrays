3d5756d32fd3660c67a84b98865a9f57b7a4edc5
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 02:29:10 +0300

    logging added

--- src/main/java/by/mahanko/array/validator/CustomArrayStringValidator.java
+++ src/main/java/by/mahanko/array/validator/CustomArrayStringValidator.java
@@ -1,14 +1,19 @@
 package by.mahanko.array.validator;
 
 import by.mahanko.array.exception.CustomException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.regex.Pattern;
 
 public class CustomArrayStringValidator {
+    private static Logger logger = LogManager.getLogger(CustomArrayStringValidator.class.getName());
     private final String regularExpression = "((0|-?[1-9][0-9]*)\s+)*(0|-?[1-9][0-9]*)";
 
     public boolean validateString(String stringToValidate) throws CustomException {
         if(stringToValidate == null || stringToValidate.length() == 0) {
+            logger.log(Level.ERROR, "Empty or null string");
             throw new CustomException();
         }
         boolean answer = Pattern.matches(regularExpression, stringToValidate);
