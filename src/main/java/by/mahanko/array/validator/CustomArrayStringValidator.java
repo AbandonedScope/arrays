32529d1ee0ee559a7f2e21b40db16fb90798a389
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 01:31:49 +0300

    regular expression and validateString method added

--- src/main/java/by/mahanko/array/validator/CustomArrayStringValidator.java
+++ src/main/java/by/mahanko/array/validator/CustomArrayStringValidator.java
@@ -1,4 +1,12 @@
 package by.mahanko.array.validator;
 
+import java.util.regex.Pattern;
+
 public class CustomArrayStringValidator {
+    private final String regularExpression = "\b(-?[1-9][0-9]*\s+)*-?[1-9][0-9]*\b";
+
+    public boolean validateString(String stringToValidate) {
+        boolean answer = Pattern.matches(regularExpression, stringToValidate);
+        return answer;
+    }
 }
