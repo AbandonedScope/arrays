4fc0133b179396e16589c43d3aee607c42d3ccba
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 01:59:50 +0300

    null string check added

--- src/main/java/by/mahanko/array/validator/CustomArrayStringValidator.java
+++ src/main/java/by/mahanko/array/validator/CustomArrayStringValidator.java
@@ -8,7 +8,7 @@ public class CustomArrayStringValidator {
     private final String regularExpression = "((0|-?[1-9][0-9]*)\s+)*(0|-?[1-9][0-9]*)";
 
     public boolean validateString(String stringToValidate) throws CustomException {
-        if(stringToValidate.length() == 0) {
+        if(stringToValidate == null || stringToValidate.length() == 0) {
             throw new CustomException();
         }
         boolean answer = Pattern.matches(regularExpression, stringToValidate);
