9e1fc85fa6814ac32253707306c0c0ce1f5446f8
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 02:29:52 +0300

    parseStringCorrectStringToParseTest, parseStringIncorrectStringToParseTest tests added

--- src/test/java/test/mahanko/array/parser/CustomStringParserTest.java
+++ src/test/java/test/mahanko/array/parser/CustomStringParserTest.java
@@ -1,4 +1,24 @@
 package test.mahanko.array.parser;
 
+import by.mahanko.array.exception.CustomException;
+import by.mahanko.array.parser.CustomStringParser;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CustomStringParserTest {
+    private CustomStringParser parser = new CustomStringParser();
+    private final String correctStringToParse = "1 -25 0 36 13 -2";
+    private final String incorrectStringToParse = "1 --25 0 36 13 -2";
+
+    @Test
+    public void parseStringCorrectStringToParseTest() throws CustomException {
+        int[] actual = parser.parseString(correctStringToParse);
+        int[] expected = {1, -25, 0, 36, 13, -2};
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test(expectedExceptions = CustomException.class)
+    public void parseStringIncorrectStringToParseTest() throws CustomException{
+        parser.parseString(incorrectStringToParse);
+    }
 }
