1870fe3b591791c98de4b498e03812352762ec86
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 02:41:01 +0300

    Unused import removed

--- src/main/java/by/mahanko/array/reader/CustomArrayTxtReader.java
+++ src/main/java/by/mahanko/array/reader/CustomArrayTxtReader.java
@@ -6,11 +6,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
