0123dcabd0db0c0cdc4d6e1c662b228e90961b30
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 02:07:50 +0300

    parseString method added

--- src/main/java/by/mahanko/array/parser/CustomStringParser.java
+++ src/main/java/by/mahanko/array/parser/CustomStringParser.java
@@ -1,4 +1,30 @@
 package by.mahanko.array.parser;
 
+import by.mahanko.array.exception.CustomException;
+import by.mahanko.array.validator.CustomArrayStringValidator;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class CustomStringParser {
+    private static Logger logger = LogManager.getLogger(CustomStringParser.class.getName());
+
+    public int[] parseString(String stringToParse) {
+        int[] result = null;
+        try {
+            CustomArrayStringValidator validator = new CustomArrayStringValidator();
+            if (validator.validateString(stringToParse)) {
+                result = Arrays.stream(stringToParse.split("\s+"))
+                        .flatMapToInt(num -> IntStream.of(Integer.parseInt(num)))
+                        .toArray();
+            }
+        } catch (CustomException exception) {
+            logger.log(Level.ERROR, "Empty or null string");
+        }
+
+        return  result;
+    }
 }
