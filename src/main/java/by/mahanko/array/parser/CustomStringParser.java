d818fe3037eb712dddd80a138f49f2f8503f204e
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 20:29:58 +0300

    Initial create, parseString method added

--- src/main/java/by/mahanko/array/parser/CustomStringParser.java
+++ src/main/java/by/mahanko/array/parser/CustomStringParser.java
@@ -1,29 +1,7 @@
 package by.mahanko.array.parser;
 
 import by.mahanko.array.exception.CustomException;
-import by.mahanko.array.validator.CustomArrayStringValidator;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
-public class CustomStringParser {
-    private static Logger logger = LogManager.getLogger(CustomStringParser.class.getName());
-
-    public int[] parseString(String stringToParse) throws CustomException {
-        int[] result = null;
-        CustomArrayStringValidator validator = new CustomArrayStringValidator();
-        if (validator.validateString(stringToParse)) {
-            result = Arrays.stream(stringToParse.split("\s+"))
-                    .flatMapToInt(num -> IntStream.of(Integer.parseInt(num)))
-                    .toArray();
-        } else {
-            logger.log(Level.ERROR, "Invalid string : " + stringToParse);
-            throw new CustomException();
-        }
-
-        return result;
-    }
+public interface CustomStringParser {
+    int[] parseString(String stringToParse) throws CustomException;
 }
