5612bc5b4983541e64633423796bb5a5dfa7b48a
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 20:22:43 +0300

    Initial create, createCustomArray method added

--- src/main/java/by/mahanko/array/creator/CustomArrayCreator.java
+++ src/main/java/by/mahanko/array/creator/CustomArrayCreator.java
@@ -1,14 +1,7 @@
 package by.mahanko.array.creator;
 
 import by.mahanko.array.entity.CustomArray;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-public class CustomArrayCreator {
-    private static Logger logger = LogManager.getLogger(CustomArrayCreator.class.getName());
-
-    public CustomArray createCustomArray(int[] array) {
-        CustomArray customArray = new CustomArray(array);
-        return customArray;
-    }
+public interface CustomArrayCreator {
+    CustomArray createCustomArray(int[] array);
 }
