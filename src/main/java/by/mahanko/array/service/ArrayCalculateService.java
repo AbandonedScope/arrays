fcf6f35f37b88120fce0d5ed689aabeaca9b7fd6
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 21:59:44 +0300

    countAverage return type changed to OptionalDouble

--- src/main/java/by/mahanko/array/service/ArrayCalculateService.java
+++ src/main/java/by/mahanko/array/service/ArrayCalculateService.java
@@ -3,8 +3,10 @@ package by.mahanko.array.service;
 import by.mahanko.array.entity.CustomArray;
 import by.mahanko.array.exception.CustomException;
 
+import java.util.OptionalDouble;
+
 public interface ArrayCalculateService {
     int countElementsSum(CustomArray array) throws CustomException;
 
-    double countAverage(CustomArray array) throws CustomException;
+    OptionalDouble countAverage(CustomArray array) throws CustomException;
 }
