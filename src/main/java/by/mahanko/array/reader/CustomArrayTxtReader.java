daf48386624795505631ce8f680f24999daf5f07
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 20:33:39 +0300

    readStringsFromFile method added

--- src/main/java/by/mahanko/array/reader/CustomArrayTxtReader.java
+++ src/main/java/by/mahanko/array/reader/CustomArrayTxtReader.java
@@ -1,4 +1,9 @@
 package by.mahanko.array.reader;
 
+import by.mahanko.array.exception.CustomFileDoesntExistException;
+
+import java.util.List;
+
 public interface CustomArrayTxtReader {
+    List<String> readStringsFromFile(String path) throws CustomFileDoesntExistException;
 }
