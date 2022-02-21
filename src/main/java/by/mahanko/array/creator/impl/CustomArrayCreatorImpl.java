18ffd15fadc9dcbd9dc838f392e3ed48fe09d417
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 20:28:37 +0300

    renamed, moved to package impl

--- src/main/java/by/mahanko/array/creator/impl/CustomArrayCreatorImpl.java
+++ src/main/java/by/mahanko/array/creator/impl/CustomArrayCreatorImpl.java
@@ -1,10 +1,11 @@
 package by.mahanko.array.creator.impl;
 
 import by.mahanko.array.entity.CustomArray;
+import by.mahanko.array.creator.CustomArrayCreator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CustomArrayCreatorImpl {
+public class CustomArrayCreatorImpl implements CustomArrayCreator{
     private static Logger logger = LogManager.getLogger(CustomArrayCreatorImpl.class.getName());
 
     public CustomArray createCustomArray(int[] array) {
