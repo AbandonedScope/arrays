7ad965118203fe1a09050b955604c43f4f02e5ea
Maxim Mahanko <tolkoo.keeper@gmail.com>
2022-02-21 20:18:52 +0300

    constructors added

--- src/main/java/by/mahanko/array/exception/CustomException.java
+++ src/main/java/by/mahanko/array/exception/CustomException.java
@@ -3,4 +3,10 @@ package by.mahanko.array.exception;
 import java.lang.Exception;
 
 public class CustomException extends Exception{
+    public CustomException() {
+        super();
+    }
+    public CustomException(String message) {
+        super(message);
+    }
 }
